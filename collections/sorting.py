b70dc77f03a3a47099a778e90ab975410d66a75f
Rolando <rolandovillca@gmail.com>
2015-10-29 23:24:31 -0400

    Adding example using collections and defaultdic factory

--- collections/sorting.py
+++ collections/sorting.py
@@ -3,17 +3,28 @@ class Person(object):
     def __init__(self, age):
         self.age = age
 
-# Create three Person objects and set the age property
-persons = [Person(age) for age in range(3) ]
-
 # Print the three created persons
-for person in persons:
-    print person.age
+def print_persons(persons):
+    for person in persons:
+        print ' ---> %s' % (person.age)
+    print ''
+
+# Create three Person objects and set the age property
+persons1 = [Person(age) for age in range(3) ]
+print_persons(persons1)
 
 # Create three Person objects and set age property
 # To use different built-in python functions
-persons = [Person(age) for age in map(lambda age: age * 10, range(3))]
+persons2 = [Person(age) for age in map(lambda age: age * 10, range(3))]
+print_persons(persons2)
+
+# Using another built-in python functions
+from collections import defaultdict
+persons_by_age = defaultdict(list)
+
+for person2 in persons2:
+    persons_by_age[person2.age].append(person2)
 
 # Print the three created persons
-for person in persons:
-    print person.age
+for person in persons_by_age:
+    print person
