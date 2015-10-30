f27519df125be792eb3008b0de07703051fbf9c6
rolandovillca <rolandovillca@gmail.com>
2015-10-30 13:29:34 -0400

    Adding comments in filtering.py file

--- collections/filtering.py
+++ collections/filtering.py
@@ -1,7 +1,10 @@
+# Fibonacci array
 fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+# Filter function to get even numbers
 result1 = filter(lambda x: x % 2, fib)
 print result1
 
+# Filter function to get numbers equal to 2
 result2 = filter(lambda x: x == 2, fib)
 print reslt2
