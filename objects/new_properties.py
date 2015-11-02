4ebb008914fed407614e5787e0df5fc74586014d
rolandovillca <rolandovillca@gmail.com>
2015-11-02 10:57:40 -0400

    Updating new_properties example, and adding comments

--- objects/new_properties.py
+++ objects/new_properties.py
@@ -1,12 +1,26 @@
-class C(object):
-    def foo(self):
-        print "Hi!"
+# Create a base class
+class MyClass(object):
+    def my_method(self):
+        print 'Into my method!'
 
-def bar(self):
-    print "Bork bork bork!"
+# Create a method
+def another_method(self):
+    print "Into another method!"
 
-c = C()
-C.bar = bar
-c.bar()
+# Create hello world method
+def hello_world(self):
+    print 'Hello World method'
 
-c.foo()
+# Adding methods and properties to an existing class
+MyClass.another_method = another_method
+MyClass.hello = hello_world
+MyClass.count = 10
+
+# Create instance object
+my_class = MyClass()
+
+# Access to all methods and properties
+my_class.my_method()
+my_class.another_method()
+my_class.hello()
+print my_class.count
