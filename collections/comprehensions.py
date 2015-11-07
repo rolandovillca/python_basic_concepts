890ab36c3d48b3a481f7fe2006870fd1e79cb9da
rolandovillca <rolandovillca@gmail.com>
2015-11-07 16:31:25 -0400

    Fixing defaultdict examples

--- collections/comprehensions.py
+++ collections/comprehensions.py
@@ -1,37 +1,33 @@
-S = {x² : x in {0 ... 9}}
-V = (1, 2, 4, 8, ..., 2¹²)
-M = {x | x in S and x even}
+from collections
 
+# Create some static values
+# S = {x² : x in {0 ... 9}}
+# V = (1, 2, 4, 8, ..., 2¹²)
+# M = {x | x in S and x even}
 
+S = [x**2 for x in range(10)]
+V = [2**i for i in range(13)]
+M = [x for x in S if x % 2 == 0]
 
->>> S = [x**2 for x in range(10)]
->>> V = [2**i for i in range(13)]
->>> M = [x for x in S if x % 2 == 0]
->>>
->>> print S; print V; print M
+print S; print V; print M
+
 [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
 [0, 4, 16, 36, 64]
 
-
->>> noprimes = [j for i in range(2, 8) for j in range(i*2, 50, i)]
->>> primes = [x for x in range(2, 50) if x not in noprimes]
->>> print primes
+noprimes = [j for i in range(2, 8) for j in range(i*2, 50, i)]
+primes = [x for x in range(2, 50) if x not in noprimes]
+print primes
 [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 
-
-
-
-
-
->>> words = 'The quick brown fox jumps over the lazy dog'.split()
->>> print words
+words = 'The quick brown fox jumps over the lazy dog'.split()
+print words
 ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog']
->>>
->>> stuff = [[w.upper(), w.lower(), len(w)] for w in words]
->>> for i in stuff:
-...     print i
-...
+
+stuff = [[w.upper(), w.lower(), len(w)] for w in words]
+for i in stuff:
+    print i
+
 ['THE', 'the', 3]
 ['QUICK', 'quick', 5]
 ['BROWN', 'brown', 5]
@@ -41,11 +37,11 @@ M = {x | x in S and x even}
 ['THE', 'the', 3]
 ['LAZY', 'lazy', 4]
 ['DOG', 'dog', 3]
->>>
->>> stuff = map(lambda w: [w.upper(), w.lower(), len(w)], words)
->>> for i in stuff:
-...     print i
-...
+
+stuff = map(lambda w: [w.upper(), w.lower(), len(w)], words)
+for i in stuff:
+    print i
+
 ['THE', 'the', 3]
 ['QUICK', 'quick', 5]
 ['BROWN', 'brown', 5]
