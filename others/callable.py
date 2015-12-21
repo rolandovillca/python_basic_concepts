b9832a17c02a35904fcfb13c20510cf638d0e5f1
rolandovillca <rolandovillca@gmail.com>
2015-12-21 17:00:36 -0400

    Adding a second example using callable

--- others/callable.py
+++ others/callable.py
@@ -1,6 +1,7 @@
 # A callable is anything that can be called.
 
-# Example 1:
+# EXAMPLE 1:
+# ==========
 class Foo:
     def __call__(self):
         print 'called'
@@ -8,7 +9,24 @@ class Foo:
 foo = Foo()
 foo()
 
-# Example 2:
+# EXAMPLE 2:
+# ==========
+# Have you ever wandered if we could make an object callable? Yes,
+# I mean just use the object name as if you were calling function! Intersted?
+class Add:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+        print "Sum of", self.num1,"and",self.num2, "is:"
+
+    def __call__(self):
+        return (self.num1 + self.num2)
+ 
+add = Add(1, 2)
+print add()
+
+# EXAMPLE 3:
+# ==========
 class Cached:
     def __init__(self, function):
         self.function = function
@@ -27,13 +45,13 @@ def ack(x, y):
 
 print ack(2, 4)
 
-# Example 3:
+# EXAMPLE 4:
+# ==========
 # A callable is an object allows you to use round parenthesis ( )
 # and eventually pass some parameters, just like functions.
 
 # Every time you define a function python creates a callable object.
 # In example, you could define the function func in these ways (it's the same):
-
 class A(object):
     def __call__(self, *args):
         print 'Hello'
