fac191d9c8212f40af10886393bb95f1798f2d9a
rolandovillca <rolandovillca@gmail.com>
2015-12-21 16:54:56 -0400

    Commit another example of callable object

--- others/callable.py
+++ others/callable.py
@@ -26,3 +26,23 @@ def ack(x, y):
     return ack(x-1, ack(x, y-1)) if x*y else (x + y + 1)
 
 print ack(2, 4)
+
+# Example 3:
+# A callable is an object allows you to use round parenthesis ( )
+# and eventually pass some parameters, just like functions.
+
+# Every time you define a function python creates a callable object.
+# In example, you could define the function func in these ways (it's the same):
+
+class A(object):
+    def __call__(self, *args):
+        print 'Hello'
+
+func = A()
+
+# or ... 
+def func(*args):
+    print 'Hello'
+
+# You could use this method instead of methods like doit or run,
+# I think it's just more clear to see obj() than obj.doit()
