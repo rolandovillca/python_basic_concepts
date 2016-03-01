f11fac97427232d6f5d87c5faec4434d7492bcf6
rolandovillca <rolandovillca@gmail.com>
2016-03-01 08:43:07 -0400

    Fix args and kargs argments examples

--- functions/args_kwargs_arguments.py
+++ functions/args_kwargs_arguments.py
@@ -1,24 +1,26 @@
-# Usage of **kwargs
-# **kwargs allows you to pass keyworded variable length of arguments to a function.
+# The **kwargs allows you to pass keyworded variable length of arguments to a function.
 # You should use **kwargs if you want to handle named arguments in a function.
-# Example:
 
 # *args = list of arguments -as positional arguments
 # **kwargs = dictionary - whose keys become separate keyword arguments and
 # the values become values of these arguments.
 
 # The syntax is the * and **. The names *args and **kwargs are only by convention but there's no hard requirement to use them.
-# You would use *args when you're not sure how many arguments might be passed to your function, i.e. it allows you pass an arbitrary number of arguments to your function.
+# You would use *args when you're not sure how many arguments might be passed to your function, 
+# i.e. it allows you pass an arbitrary number of arguments to your function.
 # Similarly, **kwargs allows you to handle named arguments that you have not defined in advance:
 
 # EXAMPLE 1:
+# ------------------------------------------------------------------------------
 def print_everything(*args):
     for count, thing in enumerate(args):
         print '{0}. {1}'.format(count, thing)
 
 print_everything('apple', 'banana', 'cabbage')
+print
 
 # EXAMPLE 2:
+# ------------------------------------------------------------------------------
 def table_things(**kwargs):
     for name, value in kwargs.items():
         print '{0} = {1}'.format(name, value)
@@ -26,6 +28,7 @@ def table_things(**kwargs):
 table_things(apple = 'fruit', cabbage = 'vegetable')
 
 # EXAMPLE 3: Using une function with 3 different types of parameters:
+# ------------------------------------------------------------------------------
 def func(arg, *args, **kwargs):
     # arg is a positional-only parameter.
     print arg
@@ -41,6 +44,8 @@ def func(arg, *args, **kwargs):
         print kwargs
 
 # func() # It will raise error related to parameters.
-func("required argument")
-func("required argument", 1, 2, '3')
-func("required argument", 1, 2, '3', keyword1=4, keyword2="foo")
+func('required argument')
+print
+func('required argument', 1, 2, '3')
+print
+func('required argument', 1, 2, '3', keyword1=4, keyword2='foo')
