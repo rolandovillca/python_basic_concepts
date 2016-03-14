3df69c569f9d70d71bf3159189dffab29fc7c752
rolandovillca <rolandovillca@gmail.com>
2016-03-14 15:49:32 -0400

    Adding mor examples with lambda functions

--- collections/lambda.py
+++ collections/lambda.py
@@ -31,3 +31,35 @@ print [(lambda x: x*x)(x) for x in range(10)]
 
 # Or much better:
 print [ x*x for x in range(10)]
+
+
+# EXAMPLE 4: Using IFS with lambda
+# ==============================================================================
+# Case 1:
+func1 = lambda x: True if x % 2 == 0 else False
+func1(100)
+func1(77)
+
+# Case 2:
+func2 = lambda x: x == 2 and x or None
+func1(5)
+func1(2)
+
+# Case 3:
+def if_else(condition, a, b) :
+    if condition :
+        return a
+    else:
+        return b
+func4 = lambda x : if_else(x>100, 'big number', 'little number')
+func1(100)
+func1(1000)
+
+# Case 4:
+func4 = lambda x: x > 100 and 'big' or 'small'
+for i in (1, 10, 99, 100, 101, 110):
+    print i, 'is', func4(i)
+
+
+# EXAMPLE 5:
+# ==============================================================================
