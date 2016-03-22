40180e1377873d90d4399b5d178c50d31365933e
rolandovillca <rolandovillca@gmail.com>
2016-03-22 16:17:27 -0400

    Rename str_vs_repr ambigous function

---
+++ others/enter_and_exit.py
@@ -0,0 +1,26 @@
+'''
+Using these magic methods (__enter__, __exit__) allows you to implement objects
+which can be used easily with the with statement.
+
+The general idea is that it makes it easy to build code which needs some 'cleandown'
+code executed (think of it as a try-finally block). Some more explanation here.
+
+A useful example could be a database connection object (which then automagically
+closes the connection once the corresponding 'with'-statement goes out of scope:
+'''
+
+class DatabaseConnection(object):
+    def __enter__(self):
+        # Make database connection and return it.
+        return self.dbconn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Make sure the database connecton gets closed.
+        self.dbconn.close()
+
+# As explained above, use this object with the with statement
+# (you may need to do from __future__ import with_statement
+# at the top of the file if you're on Python 2.5).
+
+with DatabaseConnection() as mydbconn:
+    # Do stuff.
