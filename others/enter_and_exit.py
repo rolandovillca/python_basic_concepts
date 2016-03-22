470e27fc1e4b284dd48535370db066a96848289e
rolandovillca <rolandovillca@gmail.com>
2016-03-22 17:00:49 -0400

    Adding comments for with scope chandle

--- others/enter_and_exit.py
+++ others/enter_and_exit.py
@@ -1,4 +1,12 @@
 '''
+The defining feature of a context manager is that it has two special methods:
+__enter__() and __exit__(). These are used by the with statement to enter and
+exit the context.
+
+We'll often use context managers to make transient global changes.
+This might be a change to the database transaction status or a change to the locking
+status, something that we want to do and then undo when the transaction is complete.
+
 Using these magic methods (__enter__, __exit__) allows you to implement objects
 which can be used easily with the with statement.
 
