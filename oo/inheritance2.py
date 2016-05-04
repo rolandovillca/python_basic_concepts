4dc0fb8fb8c63f817f60f4316921cde6bd43ca4d
rolandovillca <rolandovillca@gmail.com>
2016-05-04 12:23:58 -0400

    Fixing issues found in inheritance examples

--- oo/inheritance2.py
+++ oo/inheritance2.py
@@ -1,3 +1,6 @@
+# EXAMPLE 1:
+# ==============================================================================
+
 class Person(object):
     def __init__(self, name):
         self.name = name
@@ -14,11 +17,58 @@ class SuperHero(Person):
         super(SuperHero, self).reveal_identity()
         print '... And I am {}'.format(self.hero_name)
 
-# Example 1:
 corey = Person('Corey')
 corey.reveal_identity()
 print
 
-# Example 2:
 wade = SuperHero('Wade Wilson', 'Deadpool')
 wade.reveal_identity()
+print
+
+
+# EXAMPLE 2:
+# ==============================================================================
+# An inner class or nested class is a defined entirely
+# within the body of another class
+
+class Human:
+    def __init__(self):
+        self.name = 'Guido'
+        self.head = self.Head()
+
+    class Head:
+        def talk(self):
+            return 'Talking...'
+
+if __name__ == '__main__':
+    guido = Human()
+    print guido.name
+    print guido.head.talk()
+    print
+
+
+# EXAMPLE 3:
+# ==============================================================================
+# By using inner classes you can make your code even more object orientated.
+# A single object can hold several sub objects.
+# We can use them to add more structure to our programs.
+class Human2:
+
+    def __init__(self):
+        self.name = 'Guido'
+        self.head = self.Head()
+        self.brain = self.Brain()
+
+    class Head:
+        def talk(self):
+            return 'talking...'
+
+    class Brain:
+        def think(self):
+            return 'thinking...'
+
+if __name__ == '__main__':
+    guido = Human2()
+    print guido.name
+    print guido.head.talk()
+    print guido.brain.think()
