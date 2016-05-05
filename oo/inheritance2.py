2cfca5a225a40a18f1d0f589f8a42670202adcae
rolandovillca <rolandovillca@gmail.com>
2016-05-05 10:08:58 -0400

    Adding more examples about inheritance objects

--- oo/inheritance2.py
+++ oo/inheritance2.py
@@ -23,3 +23,34 @@ print
 
 wade = SuperHero('Wade Wilson', 'Deadpool')
 wade.reveal_identity()
+print
+
+
+# EXAMPLE 2: Classes can inherit functionality from other classes:
+# ==============================================================================
+
+class User:
+    name = ''
+
+    def __init__(self, name):
+        self.name = name
+
+    def print_name(self):
+        print 'Name = {}'.format(self.name)
+
+brian = User('Brian')
+brian.print_name()
+print
+
+class Programmer(User):
+    def __init__(self, name):
+        self.name = name
+
+    def do_python(self):
+        print 'programming Python'
+
+
+diana = Programmer('Diana')
+diana.print_name()
+diana.do_python()
+print
