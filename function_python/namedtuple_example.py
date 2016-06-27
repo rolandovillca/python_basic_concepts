5f65a8f56d802034918e71f13fe171c80007290e
rolandovillca <rolandovillca@gmail.com>
2016-06-27 10:02:21 -0400

    Fix documentation on named dictgionary example

--- function_python/namedtuple_example.py
+++ function_python/namedtuple_example.py
@@ -3,7 +3,8 @@
 they do not have per-instance dictionaries. Each kind of namedtuple is
 represented by its own class, created by using the namedtuple() factory function.
 
-The arguments are the name of the new class and a string containing the names of the elements.
+The arguments are the name of the new class and a string containing the names of
+the elements.
 '''
 
 # EXAMPLE 1:
