05814289e560afdc10f1422f5b3d8d6eda74aeb4
rolandovillca <rolandovillca@gmail.com>
2016-06-29 13:42:08 -0400

    Adding example 3 for matrix diagonal diference

--- hackerrank.com/03_diagonal_difference.py
+++ hackerrank.com/03_diagonal_difference.py
@@ -1,3 +1,5 @@
+#!/bin/python
+
 '''
 PROBLEM: Diagonal Difference:
 ----------------------------
@@ -18,8 +20,8 @@ a single integer.
 SAMPLE INPUT:
 ------------
 3
-11 2 4
-4 5 6
+11 2   4
+ 4 5   6
 10 8 -12
 
 SAMPLE OUTPUT:
@@ -30,14 +32,14 @@ EXPLANATION:
 -----------
 The primary diagonal is: 
 11
-      5
-            -12
+    5
+        -12
 
 Sum across the primary diagonal: 11 + 5 - 12 = 4
 
 The secondary diagonal is:
-            4
-      5
+        4
+    5
 10
 
 Sum across the secondary diagonal: 4 + 5 + 10 = 19 
@@ -47,3 +49,19 @@ Difference: |4 - 19| = 15
 import sys
 
 n = int(raw_input().strip())
+a = []
+for a_i in xrange(n):
+    a_temp = map(int,raw_input().strip().split(' '))
+    a.append(a_temp)
+
+primary_diagonal = 0
+secondary_diagonal = 0
+
+for i, rows in enumerate(a):
+    for j, row in enumerate(rows):
+        if (i - j) == 0:
+            primary_diagonal += row
+        if (((n-1) - i) - ((n-1) - j)) == 0:
+            secondary_diagonal += rows[(n-1) - i]
+
+print abs(primary_diagonal - secondary_diagonal)
