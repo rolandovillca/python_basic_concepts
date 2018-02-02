eb921c5f310c1414c8e3bbd95f6a05c9d2bf4449
Rolando Villca <rolandovillca@gmail.com>
2018-02-02 12:39:28 -0500

    Add logging example

---
+++ context_manager/log.py
@@ -0,0 +1,23 @@
+class Log:
+    def __init__(self, filename):
+        print '__init__'
+        self.filename = filename
+        self.fp = None
+
+    def logging(self, text):
+        print 'into logging method'
+        self.fp.write(text,'\n')
+
+    def __enter__(self):
+        print '__enter__'
+        self.fp = open(self.filename, 'a+')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print '__exit__'
+        self.fp.close()
+
+with Log('file.log') as logfile:
+    print 'Main'
+    logfile.logging('Test1')
+    logfile.logging('Test2')
