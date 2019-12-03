c0cb72ef1702139d6ba9eb8a3398722c455be7f3
rolandovillca <rolandovillca@gmail.com>
2019-12-03 07:27:45 -0500

    Adding nes example for factory method pattern

---
+++ patterns/creational/factory_method_serializer.py
@@ -0,0 +1,12 @@
+import json
+import xml.etree.ElementTree as et
+
+class Song:
+    def __init__(self, song_id, title, artist):
+        self.song_id = song_id
+        self.title = title
+        self.artist = artist
+
+class SongSerializer:
+    def serialize(self, song, format):
+        pass
