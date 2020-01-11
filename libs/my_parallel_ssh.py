04531660f020d744c7c6e98c050491171afede0e
rolandovillca <rolandovillca@gmail.com>
2020-01-11 15:07:19 -0500

    fix arguments

--- libs/my_parallel_ssh.py
+++ libs/my_parallel_ssh.py
@@ -1,7 +1,7 @@
 from pssh.pssh_client import ParallelSSHClient
 from pssh.exceptions import AuthenticationException, UnknownHostException, ConnectionErrorException
 
-client = ParallelSSHClient(['10.2.6.19', '10.2.6.20'], user='em7admin', password='em7admin')
+client = ParallelSSHClient(['1.1.1.1', '2.2.2.2'], user='user', password='pass')
 
 try:
     output = client.run_command('ls -ltrh ~/')
